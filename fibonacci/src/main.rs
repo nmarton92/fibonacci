911be05e9ad2aaf4660ce2e56c080b58f1390dcc
norbert.marton <norbertm921@gmail.com>
2025-02-02 18:18:05 +0100

    switch to bigint

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,18 +1,24 @@
+use uint::construct_uint;
+
+construct_uint! {
+    pub struct U256(4);
+}
+
 fn main() {
-    let mut fibonacci: Vec<u128> = Vec::new();
+    let mut fibonacci: Vec<U256> = Vec::new();
 
     loop {
         let length: usize = fibonacci.len();
 
         if length == 0 {
-            fibonacci.push(0);
+            fibonacci.push(U256::from(0));
         } else if length == 1 {
-            fibonacci.push(&fibonacci[length - 1] + 1);
+            fibonacci.push(U256::from(&fibonacci[length - 1] + 1));
         } else {
             let last = fibonacci[fibonacci.len() - 1];
             let beforeLast = fibonacci[fibonacci.len() - 2];
             
-            fibonacci.push(last + beforeLast);
+            fibonacci.push(U256::from(last + beforeLast));
         }
 
         println!("{}", fibonacci[fibonacci.len() - 1]);
