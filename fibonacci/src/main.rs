1563ccffbba4bb0fb7ce376c7f195aae9ea092a0
norbert.marton <norbertm921@gmail.com>
2025-02-02 18:28:47 +0100

    refactor generation

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -5,22 +5,21 @@ construct_uint! {
 }
 
 fn main() {
-    let mut fibonacci: Vec<U256> = Vec::new();
+    let mut fibonacci: Vec<U256> = vec![];
+
+    fibonacci.push(U256::from(0));
+    fibonacci.push(U256::from(1));
 
     loop {
         let length: usize = fibonacci.len();
 
-        if length == 0 {
-            fibonacci.push(U256::from(0));
-        } else if length == 1 {
-            fibonacci.push(U256::from(&fibonacci[length - 1] + 1));
-        } else {
-            let last = fibonacci[fibonacci.len() - 1];
-            let beforeLast = fibonacci[fibonacci.len() - 2];
-            
-            fibonacci.push(U256::from(last + beforeLast));
-        }
+        let last = fibonacci[length - 1];
+        let before_last = fibonacci[length - 2];
 
-        println!("{}", fibonacci[fibonacci.len() - 1]);
+        let next_fibonacci = U256::from(last + before_last);
+        
+        fibonacci.push(next_fibonacci);
+
+        println!("{}", next_fibonacci);
     }
 }
