d5ea31fae8f7578d5aba2ef5731562a5fc0240df
Norbert Marton <norbertm921@gmail.com>
2025-02-23 12:20:40 +0100

    test(Sequence): are first 10 consecutives fibonacce number

--- src/sequence.rs
+++ src/sequence.rs
@@ -10,3 +10,27 @@ impl ConsecutiveTerms {
         ConsecutiveTerms(self.1, next)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ConsecutiveTerms;
+
+    const REFERENCE_SEQUENCE: [u32; 10] = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+    #[test]
+    fn are_first_10_consecutives_fibonacci() {
+        let mut sequence: Vec<u32> = vec![0, 1];
+
+        for i in 0..9 {
+            let length = sequence.len();
+            let last = sequence[length - 1];
+            let before_last = sequence[length - 2];
+
+            let terms = ConsecutiveTerms(before_last, last);
+
+            sequence.push(terms.next().1);
+
+            assert_eq!(sequence.last(), Some(&REFERENCE_SEQUENCE[i]));
+        }
+    }
+}
