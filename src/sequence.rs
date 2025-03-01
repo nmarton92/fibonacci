54838d05714d93fa8beb920efbdcbb379d1db9e2
Norbert Marton <norbertm921@gmail.com>
2025-03-01 14:13:52 +0100

    feat: add backwards sequence generation

--- src/sequence.rs
+++ src/sequence.rs
@@ -1,8 +1,12 @@
 pub struct ConsecutiveTerms(pub u32, pub u32);
 
 impl ConsecutiveTerms {
-    fn nth_term_recurrence_relation(terms: &ConsecutiveTerms) -> u32 {
-        terms.0 + terms.1
+    fn nth_term_recurrence_relation(term_1: &u32, term_2: &u32) -> u32 {
+        term_1 + term_2
+    }
+
+    fn nth_term_recurrence_relation_backwards(term_1: &u32, term_2: &u32) -> u32 {
+        term_2 - term_1
     }
 
     pub fn new(term_1: Option<u32>, term_2: Option<u32>) -> ConsecutiveTerms {
@@ -13,8 +17,13 @@ impl ConsecutiveTerms {
     }
 
     pub fn next(&self) -> ConsecutiveTerms {
-        let next = ConsecutiveTerms::nth_term_recurrence_relation(self);
+        let next = ConsecutiveTerms::nth_term_recurrence_relation(&self.0, &self.1);
         ConsecutiveTerms(self.1, next)
+    }
+
+    pub fn prev(&self) -> ConsecutiveTerms {
+        let previous = ConsecutiveTerms::nth_term_recurrence_relation_backwards(&self.0, &self.1);
+        ConsecutiveTerms(previous, self.0)
     }
 }
 
@@ -47,6 +56,18 @@ mod tests {
             terms = terms.next();
 
             assert_eq!(terms.1, reference[i]);
+        }
+    }
+
+    #[test]
+    fn are_first_10_consecutives_fibonacci_backwords() {
+        let mut terms = ConsecutiveTerms::new(Some(21), Some(34));
+        let reference = [34, 21, 13, 8, 5, 3, 2, 1, 1, 0];
+
+        for i in 2..reference.len() {
+            terms = terms.prev();
+
+            assert_eq!(terms.0, reference[i]);
         }
     }
 }
