d27b80b17c287383401257e445ab51d8b372e888
Norbert Marton <norbertm921@gmail.com>
2025-03-23 18:18:25 +0100

    test(Sequence): add doc test for binet formula

--- src/formula.rs
+++ src/formula.rs
@@ -1,3 +1,11 @@
+/// Calculates the number in the sequence at the `nth` position.
+/// 
+/// # Example
+/// 
+/// ```
+/// use fibonacci::formula::binet_formula;
+/// let fifthElement = binet_formula(5); // results: 3
+/// ```
 pub fn binet_formula(nth: u32) -> u32 {
     let sqrt_of_5 = 5f64.sqrt();
     let phi = (1.0 + sqrt_of_5) / 2.0;
